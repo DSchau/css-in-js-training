9293be4c6e55dc2e1040a93c0b859414b6c62926
Dustin Schau <dustinschau@gmail.com>
2018-03-24 18:12:22 -0500

    chore: add query

--- src/templates/unit-storyboard.js
+++ src/templates/unit-storyboard.js
@@ -5,3 +5,11 @@ import Unit from './unit';
 export default function UnitStoryboard(props) {
   return <Unit storyboard={true} {...props} />;
 }
+
+export const pageQuery = graphql`
+  query UnitBySlugQueryWithStoryboard($slug: String!) {
+    unit: markdownRemark(fields: { slug: { eq: $slug } }) {
+      ...UnitFragment
+    }
+  }
+`;
